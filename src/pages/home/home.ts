939160ad781c9484d1f7d074cf5de8a0851ec4c3
Isaac Yriarte <isaacyriarte@gmail.com>
2018-03-20 23:55:21 -0500

    Se agrega el diseño de la pantalla de login y un alert en caso de exito o error en el proceso de inicio de sesion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild ("username") uname;
+  @ViewChild ("password") password;
+
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
 
+  signIn(){
+    //console.log(this.uname.value,this.password.value);
+
+    if (this.uname.value == "admin" && this.password.value == "admin"){
+      //console.log("Sucess logged in");
+      let alert = this.alertCtrl.create({
+        title: 'Login',
+        subTitle: "Sucess logged in",
+        buttons: ['OK']
+      });
+      alert.present();
+    }else{
+      //console.log("Error, the user or password is incorrect.");
+      let alert = this.alertCtrl.create({
+        title: 'Login',
+        subTitle: "Error, the user or password are incorrect.",
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+  }
 }
